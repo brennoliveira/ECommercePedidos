836510cd2533aeb96467965ed7a1db4e04b108f6
brennoliveira <brenno.oliveira@souunit.com.br>
2026-02-12 16:42:56 -0300

    feat: Add arquivo docker-compose com os services (MongoDB, SQLServer, RabbitMQ)

--- ECommercePedidos/Program.cs
+++ ECommercePedidos/Program.cs
@@ -15,7 +15,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"))
 );
 builder.Services.AddScoped<IPedidoRepository, PedidoRepository>();
 
