1c2dc4795f1c9eb358e787bb5a698152e3b358d5
brennoliveira <brenno.oliveira@souunit.com.br>
2026-02-12 10:45:01 -0300

    feat: Add CriarPedidoController

---
+++ ECommercePedidos/Controllers/PedidosController.cs
@@ -0,0 +1,26 @@
+﻿using ECommercePedidos.Application.UseCases.CriarPedido;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommercePedidos.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PedidosController(ICriarPedidoUseCase criarPedidoUseCase) : ControllerBase
+    {
+        private readonly ICriarPedidoUseCase _criarPedidoUseCase = criarPedidoUseCase;
+
+        [HttpPost]
+        public async Task<IActionResult> CriarPedido([FromBody] CriarPedidoInput input)
+        {
+            try
+            {
+                var pedidoId = await _criarPedidoUseCase.ExecutarAsync(input);
+                return CreatedAtAction(nameof(CriarPedido), new { id = pedidoId }, new { id = pedidoId });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
