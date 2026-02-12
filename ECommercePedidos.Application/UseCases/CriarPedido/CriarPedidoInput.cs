9c873e8fa85f2e49399e7ab32fbd1fd62926321c
Brenno <brenno.oliveira@souunit.com.br>
2026-02-11 22:22:52 -0300

    feat: Adicionar criar pedido useCase

---
+++ ECommercePedidos.Application/UseCases/CriarPedido/CriarPedidoInput.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommercePedidos.Application.UseCases.CriarPedido
+{
+    public class CriarPedidoInput
+    {
+        public string Cliente { get; set; } = string.Empty;
+        public List<ItemInput> Itens { get; set; } = [];
+    }
+
+    public class ItemInput
+    {
+        public string Produto { get; set; } = string.Empty;
+        public int Quantidade { get; set; }
+        public decimal PrecoUnitario { get; set; }
+    }
+}
---
+++ ECommercePedidos.Application/UseCases/CriarPedido/CriarPedidoUseCase.cs
@@ -0,0 +1,34 @@
+﻿using ECommercePedidos.Domain.Entities;
+using ECommercePedidos.Domain.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommercePedidos.Application.UseCases.CriarPedido
+{
+    public class CriarPedidoUseCase(
+        IPedidoRepository pedidoRepository,
+        IMessageBus messageBus)
+    {
+        private readonly IPedidoRepository _pedidoRepository = pedidoRepository;
+        private readonly IMessageBus _messageBus = messageBus;
+
+        public async Task<Guid> ExecutarAsync(CriarPedidoInput input)
+        {
+            var pedido = new Pedido(input.Cliente);
+
+            foreach (var item in input.Itens)
+            {
+                pedido.AdicionarItem(item.Produto, item.Quantidade, item.PrecoUnitario);
+            }
+
+            await _pedidoRepository.AdicionarPedidoAsync(pedido);
+
+            await _messageBus.PublicarPedidoCriadoAsync(pedido.Id);
+
+            return pedido.Id;
+        }
+    }
+}
